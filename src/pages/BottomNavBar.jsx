85b1d21763ec33fe141dedf40662a2a6666c0d0d
bee48x <156859901+bee48x@users.noreply.github.com>
2024-06-06 15:53:45 +0100

    profile picture

---
+++ src/pages/BottomNavBar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import { Link, useLocation } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import RewardsIcon from "@mui/icons-material/CardGiftcard";
+import ProfileIcon from "@mui/icons-material/AccountCircle";
+import Paper from "@mui/material/Paper";
+
+const BottomNavBar = () => {
+  const { pathname } = useLocation();
+  const [value, setValue] = React.useState(pathname);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  console.log(pathname);
+  return (
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation value={value} onChange={handleChange} showLabels>
+        <BottomNavigationAction
+          label="Rewards"
+          value="/rewards"
+          icon={<RewardsIcon />}
+          component={Link}
+          to="/rewards"
+        />
+        <BottomNavigationAction
+          label="Home"
+          value="/"
+          icon={<HomeIcon />}
+          component={Link}
+          to="/"
+        />
+        <BottomNavigationAction
+          label="Profile"
+          value="/profile"
+          icon={<ProfileIcon />}
+          component={Link}
+          to="/profile"
+          focusRipple={true}
+        />
+      </BottomNavigation>
+    </Paper>
+  );
+};
+
+export default BottomNavBar;
