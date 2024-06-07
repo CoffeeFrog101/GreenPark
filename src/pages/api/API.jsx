442cae19d16b33cb72bcb7552b4971cbff4603e7
T0170536 <78859835+CoffeeFrog101@users.noreply.github.com>
2024-06-07 14:15:50 +0100

    fixed issues

---
+++ src/pages/api/API.jsx
@@ -0,0 +1,38 @@
+const fetchData = async (API) => {
+  const response = await fetch(API);
+  const responseData = await response.json();
+  let displayData;
+
+  displayData = responseData.carParks.carPark
+    .map(function (mapper) {
+      const parkRide =
+        mapper.definition.parkingRecord.parkingRecord.parkingName.values.value[
+          "#text"
+        ];
+      const numOfSpace =
+        mapper.definition.parkingRecord.parkingRecord.parkingNumberOfSpaces;
+      const status =
+        mapper.status.parkingRecord.parkingRecordStatus
+          .parkingSiteOpeningStatus;
+      const position = [
+        mapper.definition.parkingRecord.parkingRecord.parkingLocation
+          .locationForDisplay["latitude"],
+        mapper.definition.parkingRecord.parkingRecord.parkingLocation
+          .locationForDisplay["longitude"],
+      ];
+
+      if (parkRide.includes("Park and Ride")) {
+        return {
+          id: mapper.id,
+          name: parkRide,
+          capacity: numOfSpace,
+          status: status,
+          position: position,
+        };
+      }
+      return null;
+    })
+    .filter(Boolean);
+  return displayData;
+};
+export default fetchData;
